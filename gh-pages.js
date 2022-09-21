f9db269ce0d8cbf37155a378bd5a9fb4794d4b43
Arjun <askalburgi@gmail.com>
2022-09-20 22:02:15 -0400

    deployment capability to gh-pages

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+import { publish } from 'gh-pages';
+
+publish(
+ 'build', // path to public directory
+ {
+  branch: 'gh-pages',
+  repo: 'https://github.com/arjunkalburgi/2022-website.git',
+  user: {
+   name: 'Arjun Kalburgi',
+   email: 'askalburgi@gmail.com'
+  },
+  dotfiles: true
+  },
+  () => {
+   console.log('Deploy Complete!');
+  }
+);
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		paths: {
+			base: dev ? '' : '/2022-website',
+		},
+
 	}
 };
 
